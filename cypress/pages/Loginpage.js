0ee37a35eeefaa7f5235135fc12cf3e6ef049cd8
FilipSavu <savufilip@gmail.com>
2025-01-14 20:16:15 +0200

    add construcotr

--- cypress/pages/Loginpage.js
+++ cypress/pages/Loginpage.js
@@ -1,18 +1,29 @@
 class LoginPage{
 
+    constructor(){
+     this.emailfield="#user_email"
+
+    }
+
 NavigateToLoginPage(){
     cy.viewport(1920,1080);
     cy.visit("https://www.edu.goit.global/account/login");
 }
 
 VerifyLoginPage(){
-    cy.get("div[class='next-10stgr7 e1jenigf0']>h2[class='next-c1vj7d e168p23h3']").should("be.visible");
-    cy.get("div[class='next-10stgr7 e1jenigf0']>h2[class='next-c1vj7d e168p23h3']").should("have.text","Login");
+    cy.get(".next-10stgr7 > .next-c1vj7d ").should("be.visible");
+    cy.get(".next-10stgr7 > .next-c1vj7d ").should("have.text","Login");
 }
 
 
 VerifyEmailField(){
-    cy.get("#user_email").should("be.visible");
+    cy.get(this.emailfield).should("be.visible");
+}
+
+
+inputEmail(email){
+    cy.get(this.emailfield).type(email)
+
 }
 
 VerifyPassowrdField(){
