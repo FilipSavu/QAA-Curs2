2bdddbbc83e5f32755b0d2aaa40bba7204a9e4a3
FilipSavu <savufilip@gmail.com>
2025-01-15 16:29:33 +0200

    Update homepage.js

---
+++ cypress/pages/homepage.js
@@ -0,0 +1,38 @@
+class Homepage{
+
+    constructor(){
+     this.emailfield="#user_email"
+     this.logintitle=".next-10stgr7 > .next-c1vj7d "
+     this.userpassword="#user_password"
+     this.submitbutton="button[type='submit']"
+    }
+
+NavigateToLoginPage(){
+    cy.viewport(1920,1080);
+    cy.visit("https://www.edu.goit.global/account/login");
+}
+VerifyLoginPage(){
+    cy.get(this.logintitle).should("be.visible");
+    cy.get(this.logintitle).should("have.text","Login");
+}
+VerifyEmailField(){
+    cy.get(this.emailfield).should("be.visible");
+}
+inputEmail(email){
+    cy.get(this.emailfield).type(email);
+}
+inputPassword(password){
+    cy.get(this.userpassword).type(password);
+}
+VerifyPassowrdField(){
+    cy.get(this.userpassword).should("be.visible");
+}
+VerifySubmitButtin(){
+    cy.get(this.submitbutton).should("be.visible");
+}
+clickLoginButton(){
+    cy.get(this.submitbutton).click();
+
+}
+}
+export default  Homepage;
