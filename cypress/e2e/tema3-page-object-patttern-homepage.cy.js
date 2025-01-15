81562c4ed19ba85208819c462cb4311099488b91
FilipSavu <savufilip@gmail.com>
2025-01-15 16:38:12 +0200

    Update tema3-page-object-patttern-homepage.cy.js

--- cypress/e2e/tema3-page-object-patttern-homepage.cy.js
+++ cypress/e2e/tema3-page-object-patttern-homepage.cy.js
@@ -4,16 +4,10 @@ describe("login test",()=>{
 
     it("visit goit login page", ()=>{
       
-        const PaginaStart = new Homepage();
+        const login=new Homepage ()
 
-        login.NavigateToLoginPage();
-        login.VerifyEmailField();
-        login.VerifyLoginPage();
-        login.VerifyPassowrdField();
-        login.VerifySubmitButtin();
-        login.inputEmail("testowyqa@qa.team");
-        login.inputPassword("QA!automation-1");
-        login.clickLoginButton();
+        Homepage.NavigateToLoginPage();
+
              
         });
     });
