d9ba40e84513d2033e4f5d12a9a989c5ccb50cd6
FilipSavu <savufilip@gmail.com>
2025-01-15 16:15:23 +0200

    creare homepage si test homepage

---
+++ cypress/e2e/tema3-page-object-patttern-homepage.cy.js
@@ -0,0 +1,19 @@
+import Homepage from "../pages/homepage";
+
+describe("login test",()=>{
+
+    it("visit goit login page", ()=>{
+      
+        const login=new Login ()
+
+        login.NavigateToLoginPage();
+        login.VerifyEmailField();
+        login.VerifyLoginPage();
+        login.VerifyPassowrdField();
+        login.VerifySubmitButtin();
+        login.inputEmail("user888@gmail.com");
+        login.inputPassword("1234567890");
+        login.clickLoginButton();
+             
+        });
+    });
